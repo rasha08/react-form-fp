f331f4e67a755a0aa7defcd936389e5720db3bb1
rasha08 <rasha08@gmail.com>
2021-01-16 13:06:06 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-form-fp/precache-manifest.809a65ff27e40bd18817710ced3cb405.js"
+  "/react-form-fp/precache-manifest.092362bf96447ac7a269b0a92540ad6b.js"
 );
 
 self.addEventListener('message', (event) => {
